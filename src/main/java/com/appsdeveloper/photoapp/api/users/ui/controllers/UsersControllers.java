66fadc85e4e3ca4315ce74ae8e52457eec2fcc43
Mohd Nafay <mohd.nafay@hpe.com>
2021-05-05 18:03:36 +0530

    Initial commit PhotoAppApiUsers

---
+++ src/main/java/com/appsdeveloper/photoapp/api/users/ui/controllers/UsersControllers.java
@@ -0,0 +1,16 @@
+package com.appsdeveloper.photoapp.api.users.ui.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersControllers {
+
+	@GetMapping("/status/check")
+	public String healthCheck() {
+		return "Service is up and running";
+
+	}
+}
