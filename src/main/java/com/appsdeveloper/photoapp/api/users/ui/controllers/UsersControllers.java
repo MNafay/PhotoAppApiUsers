0597150e7bd3534283e3db9d908c3cf26e44251a
Mohd Nafay <mohd.nafay@hpe.com>
2021-05-30 15:22:05 +0530

    Added Environment variable

--- src/main/java/com/appsdeveloper/photoapp/api/users/ui/controllers/UsersControllers.java
+++ src/main/java/com/appsdeveloper/photoapp/api/users/ui/controllers/UsersControllers.java
@@ -1,5 +1,7 @@
 package com.appsdeveloper.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersControllers {
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/status/check")
 	public String healthCheck() {
-		return "Service is up and running";
+		return "Service is up and running :"+env.getProperty("local.server.port");
 
 	}
 }
